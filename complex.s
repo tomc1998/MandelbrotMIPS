7e7fbda5c85abcb0ace32a844666fe68423a1a07
Thomas Cheng <thomascheng1998@googlemail.com>
2016-12-06 00:35:49 +0000

    Added support for complex numbers

---
+++ complex.s
@@ -0,0 +1,85 @@
+#############
+# This file contains functions to deal with complex numbers.
+#############
+
+#########################################
+# Function complex_add
+# Adds 2 complex numbers together
+# Args: 
+# 4b (Floating point real part A)
+# 4b (Floating point imag part A)
+# 4b (Floating point real part B)
+# 4b (Floating point imag part B)
+# Returns:
+# f0 = Floating point real part result
+# f1 = Floating point imag part result
+.globl complex_add
+complex_add:
+  l.s $f4, 12($sp) # Areal
+  l.s $f5, 8($sp)  # Aimag
+  l.s $f6, 4($sp)  # Breal
+  l.s $f7, 0($sp)  # Bimag
+  add.s $f0, $f4, $f6
+  add.s $f1, $f5, $f7
+  jr $ra
+
+#########################################
+# Function complex_mult
+# Multiplies 2 complex numbers
+# Args: 
+# 4b (Floating point real part A)
+# 4b (Floating point imag part A)
+# 4b (Floating point real part B)
+# 4b (Floating point imag part B)
+# Returns:
+# f0 = Floating point real part result
+# f1 = Floating point imag part result
+.globl complex_mult
+complex_mult:
+  l.s $f4, 12($sp)   # Areal
+  l.s $f5, 8($sp)   # Aimag
+  l.s $f6, 4($sp)   # Breal
+  l.s $f7, 0($sp)  # Bimag
+  # Calculate real part
+  mul.s $f0, $f4, $f6 # Real mult
+  mul.s $f8, $f5, $f7 # Imag mult
+  sub.s $f0, $f0, $f8
+  # Calculate imaginary part
+  mul.s $f1, $f4, $f7 
+  mul.s $f8, $f5, $f6 
+  add.s $f1, $f1, $f8
+  jr $ra
+
+#########################################
+# Function complex_sq
+# Squares a complex number
+# Args: 
+# 4b (Floating point real part)
+# 4b (Floating point imag part)
+# Returns:
+# f0 = Floating point real part result
+# f1 = Floating point imag part result
+.globl complex_sq
+complex_sq:
+  l.s $f4, 4($sp)   # Areal
+  l.s $f5, 0($sp)   # Aimag
+  # Push ra to stack
+  addi $sp, $sp, -4
+  sw $ra, 0($sp)
+  # Push to stack to call complex_mult
+  addi $sp, $sp, -16
+  s.s $f4, 12($sp)
+  s.s $f5, 8($sp)
+  s.s $f4, 4($sp)
+  s.s $f5, 0($sp)
+  jal complex_mult
+  addi $sp, $sp, 16
+  # Restore $ra
+  lw $ra, 0($sp)
+  addi $sp, $sp, 4
+  jr $ra
+  
+  
+  
+  
+
--- main.s
+++ main.s
@@ -1,6 +1,10 @@
 .data
 str:  .asciiz "Hello, world!\n"
 dest: .space  128
+ar: .float 1.0
+ai: .float 1.0
+br: .float 2.0
+bi: .float 1.0
 
 .text
 
@@ -14,6 +18,24 @@ main:
   la $t1, framebuffer
   sb $t0, 10($t1)
   jal render
+
+  # Test complex
+  # a = 1 + i
+  # b = 2 + i
+  # ab = (1+i)(2+i) = 2 + i + 2i - 1 = 1 + 3i
+  l.s $f4, ar
+  l.s $f5, ai
+  # Push values onto stack
+  addi $sp, $sp, -8
+  s.s $f4, 4($sp)   # Breal
+  s.s $f5, 0($sp)   # Bimag
+  jal complex_sq
+  addi $sp, $sp, 8
+  li $v0, 2
+  mov.s $f12, $f0
+  syscall
+  mov.s $f12, $f1
+  syscall
 
   li $v0, 10
   syscall
